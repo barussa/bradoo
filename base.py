d8a95c8982c4fbd7f31e1e6ad8a92fc5fcf2a462
codefarras <codeope@farrasapp.com>
2020-06-29 23:59:33 -0300

    gitignore

--- base.py
+++ base.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    'postgresql://henrique_gfc:hbcm@localhost:5432/henrique_gfc'
+    'postgresql://postgres:postgres@127.0.0.1:5432/vendor'
 )
 Session = sessionmaker(bind=engine)
 
