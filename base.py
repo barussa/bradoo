808c1f3a9e79e3c6275efaadd83c92c127930093
codefarras <codeope@farrasapp.com>
2020-07-01 17:35:20 -0300

    updated

--- base.py
+++ base.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os 
-
-engine = create_engine(
-    'postgresql://postgres:postgres@127.0.0.1:5432/vendor'
-)
+DATABASE_URL = os.environ['DATABASE_URL']
+print(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
 
