2ca315d43d9aceed34b38e61cfff28642aabc717
codefarras <codeope@farrasapp.com>
2020-06-30 23:45:57 -0300

    print DB

--- base.py
+++ base.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os 
 DATABASE_URL = os.environ['DATABASE_URL']
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
